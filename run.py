0e6c89bebd52e21540a83ce5be14e06e6ddf9c10
anothermartz <89461919+anothermartz@users.noreply.github.com>
2023-10-27 22:14:11 +0100

    added use_previous_tracking_data
    Tick this to force to retrack the current video.

--- run.py
+++ run.py
@@ -23,6 +23,7 @@ vocal_file = config['OPTIONS']['vocal_file']
 quality = config['OPTIONS']['quality']
 output_height = config['OPTIONS']['output_height']
 wav2lip_version = config['OPTIONS']['wav2lip_version']
+use_previous_tracking_data = config['OPTIONS']['use_previous_tracking_data']
 nosmooth = config.getboolean('OPTIONS', 'nosmooth')
 U = config.getint('PADDING', 'U')
 D = config.getint('PADDING', 'D')
@@ -258,7 +259,7 @@ while True:
   if os.path.isfile('last_file.txt'):
     with open('last_file.txt', 'r') as file:
       last_file = file.readline()
-    if last_file != temp_input_video:
+    if last_file != temp_input_video or use_previous_tracking_data == False:
         if os.path.isfile(last_detected_face):
           os.remove(last_detected_face)
 
